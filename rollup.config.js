f8095052684438d31125bfa155df8a171be960d9
XIE CHEN <xiechen0692@gmail.com>
2022-11-28 00:52:17 +0900

    update config

--- rollup.config.js
+++ rollup.config.js
@@ -7,7 +7,7 @@ import typescript from "@rollup/plugin-typescript";
 
 export default [
   {
-    input: "./src/index.ts",
+    input: "./src/index.tsx",
     output: [
       {
         file: "dist/index.js",
